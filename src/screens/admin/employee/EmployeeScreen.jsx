28d13599a57dd97f26eb698b167d99d98fd56827
SuosPhearith <suosphearith@gmail.com>
2024-05-30 11:13:06 +0700

    finished employee screen

--- src/screens/admin/employee/EmployeeScreen.jsx
+++ src/screens/admin/employee/EmployeeScreen.jsx
@@ -15,7 +15,15 @@ import {
   RiDeleteBinFill,
   RiToggleLine,
 } from "react-icons/ri";
-import { Input, Modal, Pagination, Select, Space, message } from "antd";
+import {
+  Input,
+  Modal,
+  Pagination,
+  Popconfirm,
+  Select,
+  Space,
+  message,
+} from "antd";
 //::================================================================================::
 
 //::===============================>>Custom library<<===============================::
@@ -25,6 +33,7 @@ import "./EmployeeScreen.scss";
 import makeAPIRequest from "../../../services/makeAPIRequest";
 import SpinLayout from "../../../components/general/spin/SpinLayout";
 //::================================================================================::
+const imageUrl = import.meta.env.VITE_IMAGE_URL;
 
 const gender = [
   {
@@ -45,6 +54,16 @@ const UserScreen = () => {
     totalPages: 1,
   });
   const [search, setSearch] = useState("");
+  const [createModal, setCreateModal] = useState(false);
+  const [updateModal, setUpdateModal] = useState(false);
+  const [getOneModal, setGetOneModal] = useState(false);
+  const [updateId, setUpdateId] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
   //::===============================>>Screen action<<================================::
   const getUsers = async () => {
     try {
@@ -60,37 +79,129 @@ const UserScreen = () => {
         totalPages: data.totalPages,
       });
     } catch (error) {
-      message.error("Something error");
+      message.error("Something went wrong");
+    }
+  };
+  const findOne = async (id) => {
+    try {
+      const employee = await makeAPIRequest("GET", `employee/${id}`);
+      if (employee) {
+        setFormData({
+          name: employee.name,
+          email: employee.email,
+          gender: employee.gender,
+        });
+      }
+    } catch (error) {
+      message.error("Something went wrong");
+    }
+  };
+  const removeEmployee = async (id) => {
+    try {
+      if (!id) return message.info("Update ID not found");
+      const feedback = await makeAPIRequest("DELETE", `employee/${id}`);
+      if (feedback) {
+        message.success("Deleted successfully");
+        getUsers();
+      }
+    } catch (error) {
+      message.error("Something went wrong");
+    }
+  };
+  const toggleActiveEmployee = async (id) => {
+    try {
+      if (!id) return message.info("Update ID not found");
+      const feedback = await makeAPIRequest(
+        "PATCH",
+        `employee/${id}/toggle-active`
+      );
+      if (feedback) {
+        getUsers();
+      }
+    } catch (error) {
+      message.error("Something went wrong");
+    }
+  };
+  const updateEmployee = async () => {
+    try {
+      if (!updateId) return message.info("Update ID not found");
+      const { name, email, gender } = formData;
+      if (!name || !email || !gender)
+        return message.info("Please input all fields");
+      const feedback = await makeAPIRequest("PATCH", `employee/${updateId}`, {
+        name,
+        email,
+        gender,
+      });
+      if (feedback) {
+        getUsers();
+        setUpdateModal(false);
+        setUpdateId("");
+        message.success("Updated successfully");
+      }
+    } catch (error) {
+      message.error("Something went wrong");
+    }
+  };
+  const createEmployee = async () => {
+    try {
+      const { name, email, password, gender } = formData;
+      if (!name || !email || !password || !gender)
+        return message.info("Please input all fields");
+      const feedback = await makeAPIRequest("POST", "employee", {
+        name,
+        email,
+        password,
+        gender,
+      });
+      if (feedback) {
+        setCreateModal(false);
+        setFormData({
+          name: "",
+          email: "",
+          password: "",
+          gender: "",
+        });
+        message.success("Created successfully");
+        getUsers();
+      }
+    } catch (error) {
+      message.error("Something went wrong");
     }
   };
   useEffect(() => {
-    getUsers();
+    getUsers(); // Call getUsers initially when the component mounts
+  }, []); // Add an empty dependency array to ensure this effect runs only once
+
+  useEffect(() => {
+    getUsers(); // Call getUsers when pagination.currentPage changes
   }, [pagination.currentPage]);
+
+  useEffect(() => {
+    getUsers(); // Call getUsers when search term changes
+  }, [search]);
   const paginationChange = (current, size) => {
     setPagination({ ...pagination, currentPage: current });
     console.log(`${current}, ${size}`);
   };
   //::================================================================================::
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    gender: "",
-  });
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+  const handleFormSubmit = () => {
+    createEmployee();
   };
   const handleFormCancel = () => {
+    setCreateModal(false);
     setFormData({
       name: "",
       email: "",
       password: "",
-      role: "",
       gender: "",
     });
+  };
+  const handleUpdate = (id) => {
+    findOne(id);
+    setUpdateModal(true);
+    setUpdateId(id);
   };
 
   return (
@@ -123,7 +234,7 @@ const UserScreen = () => {
               </div>
               <div
                 className="admin-user-header-right-create"
-                onClick={() => setIsModalOpen(true)}
+                onClick={() => setCreateModal(true)}
               >
                 <RiAddCircleLine size={29} className="create-btn-hover" />
               </div>
@@ -145,30 +256,52 @@ const UserScreen = () => {
                 <div className="name">{user.name}</div>
                 <div className="email">{user.email}</div>
                 <div className="active-emp">
-                  {user.active ? (
-                    <button className="search-btn">
-                      <RiToggleLine size={30} />
+                  {user.status ? (
+                    <button
+                      className="search-btn"
+                      onClick={() => toggleActiveEmployee(user.id)}
+                    >
+                      <RiToggleFill size={30} />
                     </button>
                   ) : (
-                    <button className="search-btn">
-                      <RiToggleFill size={30} />
+                    <button
+                      className="search-btn"
+                      onClick={() => toggleActiveEmployee(user.id)}
+                    >
+                      <RiToggleLine size={30} />
                     </button>
                   )}
                 </div>
                 <div className="avatar-emp">
-                  <img src={user.avatar} alt="" />
+                  <img src={`${imageUrl}${user.avatar}`} alt="" />
                 </div>
                 <div className="gender-emp">{user.gender.toUpperCase()}</div>
                 <div className="action-emp">
                   <div className="action-inside">
-                    <button className="search-btn btn-action">
+                    <button
+                      className="search-btn btn-action"
+                      onClick={() => (setGetOneModal(true), findOne(user.id))}
+                    >
                       <RiEyeFill size={20} />
                     </button>
-                    <button className="btn-action">
+                    <button
+                      className="btn-action"
+                      onClick={() => handleUpdate(user.id)}
+                    >
                       <RiEditFill size={20} color="blue" />
                     </button>
                     <button className="btn-action">
-                      <RiDeleteBinFill size={20} color="red" />
+                      <Popconfirm
+                        title="Remove employee"
+                        description="Are you sure to remove this employee?"
+                        onConfirm={() => removeEmployee(user.id)}
+                        okText="Yes"
+                        cancelText="No"
+                      >
+                        <button className="btn-action">
+                          <RiDeleteBinFill size={20} color="red" />
+                        </button>
+                      </Popconfirm>
                     </button>
                   </div>
                 </div>
@@ -190,74 +323,177 @@ const UserScreen = () => {
 
           {/* start create modal */}
           <Modal
-            title="Create User"
-            open={isModalOpen}
-            onOk={() => setIsModalOpen(false)}
-            onCancel={() => setIsModalOpen(false)}
+            title="Create Employee"
+            open={createModal}
+            onOk={() => setCreateModal(false)}
+            onCancel={() => setCreateModal(false)}
             footer={false}
           >
             <section className="my-form-section">
-              <form onSubmit={handleFormSubmit}>
-                <Space style={{ width: "100%" }} direction="vertical">
-                  <p style={{ color: "grey" }}>
-                    Name <span style={{ color: "red" }}>*</span>
-                  </p>
-                  <Input
-                    placeholder="Name"
-                    value={formData.name}
-                    onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
-                    }
-                  />
-                  <p style={{ color: "grey" }}>
-                    Email <span style={{ color: "red" }}>*</span>
-                  </p>
-                  <Input
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={(e) =>
-                      setFormData({ ...formData, email: e.target.value })
-                    }
-                  />
-                  <p style={{ color: "grey" }}>
-                    Password <span style={{ color: "red" }}>*</span>
-                  </p>
-                  <Input.Password
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={(e) =>
-                      setFormData({ ...formData, password: e.target.value })
-                    }
-                  />
-                  <p style={{ color: "grey" }}>
-                    Gender <span style={{ color: "red" }}>*</span>
-                  </p>
-                  <Select
-                    placeholder="Gender"
-                    onChange={(e) =>
-                      setFormData({ ...formData, gender: e.target.value })
-                    }
-                    style={{
-                      width: "100%",
-                    }}
-                    options={gender}
-                  />
+              <Space style={{ width: "100%" }} direction="vertical">
+                <p style={{ color: "grey" }}>
+                  Name <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+                <p style={{ color: "grey" }}>
+                  Email <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+                <p style={{ color: "grey" }}>
+                  Password <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input.Password
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <p style={{ color: "grey" }}>
+                  Gender <span style={{ color: "red" }}>*</span>
+                </p>
+                <Select
+                  placeholder="Gender"
+                  value={formData.gender}
+                  onChange={(value) => {
+                    setFormData({ ...formData, gender: value });
+                  }}
+                  style={{
+                    width: "100%",
+                  }}
+                  options={gender}
+                />
+              </Space>
+              <div className="btn-create-form">
+                <Space>
+                  <MyButton
+                    color="whitesmoke"
+                    textColor="black"
+                    onClick={handleFormCancel}
+                  >
+                    Cancel
+                  </MyButton>
+                  <MyButton
+                    color="white"
+                    textColor="black"
+                    onClick={handleFormSubmit}
+                  >
+                    Create
+                  </MyButton>
                 </Space>
-                <div className="btn-create-form">
-                  <Space>
-                    <MyButton
-                      color="blue"
-                      textColor="white"
-                      onClick={handleFormCancel}
-                    >
-                      Cancel
-                    </MyButton>
-                    <MyButton color="white" textColor="black" type="submit">
-                      Create
-                    </MyButton>
-                  </Space>
-                </div>
-              </form>
+              </div>
+            </section>
+          </Modal>
+          {/* end create modal */}
+          {/* start update modal */}
+          <Modal
+            title="Update Employee"
+            open={updateModal}
+            onOk={() => setUpdateModal(false)}
+            onCancel={() => setUpdateModal(false)}
+            footer={false}
+          >
+            <section className="my-form-section">
+              <Space style={{ width: "100%" }} direction="vertical">
+                <p style={{ color: "grey" }}>
+                  Name <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+                <p style={{ color: "grey" }}>
+                  Email <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+                <p style={{ color: "grey" }}>
+                  Gender <span style={{ color: "red" }}>*</span>
+                </p>
+                <Select
+                  placeholder="Gender"
+                  value={formData.gender}
+                  onChange={(value) => {
+                    setFormData({ ...formData, gender: value });
+                  }}
+                  style={{
+                    width: "100%",
+                  }}
+                  options={gender}
+                />
+              </Space>
+              <div className="btn-create-form">
+                <Space>
+                  <MyButton
+                    color="whitesmoke"
+                    textColor="black"
+                    onClick={() => (
+                      setUpdateModal(false),
+                      setFormData({
+                        name: "",
+                        email: "",
+                        password: "",
+                        gender: "",
+                      })
+                    )}
+                  >
+                    Cancel
+                  </MyButton>
+                  <MyButton
+                    color="white"
+                    textColor="black"
+                    onClick={() => updateEmployee()}
+                  >
+                    Update
+                  </MyButton>
+                </Space>
+              </div>
+            </section>
+          </Modal>
+          {/* end create modal */}
+          {/* start get one modal */}
+          <Modal
+            title="Employee"
+            open={getOneModal}
+            onOk={() => setGetOneModal(false)}
+            onCancel={() => setGetOneModal(false)}
+            footer={false}
+          >
+            <section className="my-form-section">
+              <Space style={{ width: "100%" }} direction="vertical">
+                <p style={{ color: "grey" }}>
+                  Name <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input placeholder="Name" value={formData.name} />
+                <p style={{ color: "grey" }}>
+                  Email <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input placeholder="Email" value={formData.email} />
+                <p style={{ color: "grey" }}>
+                  Gender <span style={{ color: "red" }}>*</span>
+                </p>
+                <Input placeholder="Email" value={formData.gender} />
+              </Space>
             </section>
           </Modal>
           {/* end create modal */}
